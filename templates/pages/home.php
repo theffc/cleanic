e5d716d628eab70fcec4657e04a7102909b81c80
Lucas Borges Fernandes <lucasbfernandes94@gmail.com>
2017-06-25 17:45:31 -0300

    removendo arquivos desnecessarios - adicionando jquery e js bootstrap para o projeto

--- templates/pages/home.php
+++ templates/pages/home.php
@@ -6,6 +6,9 @@
 
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php require_once(__TEMPLATES__ . '/components/styles/styles-0001.php'); ?>
 	</head>
 
@@ -13,5 +16,7 @@
 		<?php require_once(__TEMPLATES__ . '/components/header/header-0001.php'); ?>
 
 		<?php require_once(__TEMPLATES__ . '/components/footer/footer-0001.php'); ?>
+
+		<?php require_once(__TEMPLATES__ . '/components/scripts/scripts-0001.php'); ?>
 	</body>
 </html>
