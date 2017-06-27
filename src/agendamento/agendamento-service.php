dc0e7ba6d4deaf821418274edd79a170029bb2fb
Braz <pedroaugusto@alfaebeto.org.br>
2017-06-27 18:39:57 -0300

    [UPDATE] Serviço para consulta de horários disponíveis, de acordo médico e data.

--- src/agendamento/agendamento-service.php
+++ src/agendamento/agendamento-service.php
@@ -3,7 +3,7 @@ require_once '../../config/defaults.php';
 require_once 'agendamento.php';
 require_once '../customer/customer-service.php';
 require_once 'AgendamentoModificado.php';
-
+require_once 'horarios-disponiveis.php';
 
 #return the appointments with Medics only, contains Medic name, speciality, pacient name and phone number
 function ListaAgendamento() {
@@ -61,7 +61,57 @@ class RequestNewAgendamento
    }
 }
 
+function ListaHorariosDisponiveis($idMedico, $dataAgendamento){
 
+   $query = "SELECT horaAgendamento FROM Agenda AS A, Funcionario as M WHERE A.codFuncionario = '$idMedico' AND A.dataAgendamento = '$dataAgendamento' AND M.idFunc = '$idMedico' AND M.cargoFunc = 'MEDICO'";
+   $db = connectToDatabase();
+   $result = $db->query($query);
+   if ($result == NULL) {
+      return NULL;
+   }
+   else{
+
+      $disponibilidades = array();
+      for ($i = 0; $i < 10; $i++){
+         $disponibilidades[$i] = true;
+      }
+
+      while ($assoc = $result->fetch_assoc()) {
+         if ($assoc['horaAgendamento'] == "08:00:00"){
+            $disponibilidades[0] = false;
+         }
+         else if ($assoc['horaAgendamento'] == "09:00:00"){
+            $disponibilidades[1] = false;
+         }
+         else if ($assoc['horaAgendamento'] == "10:00:00"){
+            $disponibilidades[2] = false;
+         }
+         else if ($assoc['horaAgendamento'] == "11:00:00"){
+            $disponibilidades[3] = false;
+         }
+         else if ($assoc['horaAgendamento'] == "12:00:00"){
+            $disponibilidades[4] = false;
+         }
+         else if ($assoc['horaAgendamento'] == "13:00:00"){
+            $disponibilidades[5] = false;
+         }
+         else if ($assoc['horaAgendamento'] == "14:00:00"){
+            $disponibilidades[6] = false;
+         }
+         else if ($assoc['horaAgendamento'] == "15:00:00"){
+            $disponibilidades[7] = false;
+         }
+         else if ($assoc['horaAgendamento'] == "16:00:00"){
+            $disponibilidades[8] = false;
+         }
+         else if ($assoc['horaAgendamento'] == "17:00:00"){
+            $disponibilidades[9] = false;
+         }
+      }
+      $horariosDisponiveis = new horariosDisponiveis($disponibilidades);
+      return json_encode($horariosDisponiveis);
+   }
+}
 
 function testAddAgendamento() {
    $request = new RequestNewAgendamento(array('dataAgendamento' => '2017-06-26', 'horaAgendamento' => '12:00:00', 'codFuncionario' => 2, 'nomePac' => 'TestandoAgendamento', 'telPac' => '99994933'));
@@ -69,5 +119,4 @@ function testAddAgendamento() {
    print_r($agendamento);
    echo json_encode($agendamento);
 }
-
 ?>
