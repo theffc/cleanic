1c84b7aeac94c3d960e354bdced484ff97d9fded
theffc <fredericoffc@gmail.com>
2017-06-28 18:43:34 -0300

    fix: admin page

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,4 +1,12 @@
 <?php 
+	session_start();
+
 	require_once(dirname(dirname(dirname(__FILE__))) . '/config/constants.php');
-	require_once(__TEMPLATES__ . '/pages/admin.php');
+
+	if (isset($_SESSION['user'])) {
+		require_once(__TEMPLATES__ . '/pages/admin.php');
+	} else {
+		require_once(__TEMPLATES__ . '/pages/home.php');
+		exit;
+	}
 ?>
