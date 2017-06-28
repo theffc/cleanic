988488590ef878807e2eecef5bc80081aa1bc597
Braz <pedroaugusto@alfaebeto.org.br>
2017-06-28 17:30:41 -0300

    Proteção contra SQL Injection no Login

--- src/user/user-service.php
+++ src/user/user-service.php
@@ -3,19 +3,25 @@
 require_once  '../../config/defaults.php';
 require_once 'user.php';
 
-
 function login(Usuario $user) {
    $conn = connectToDatabase();
    if (!$conn) {
       return NULL;
    }
 
-   $query = "SELECT username, password FROM Usuario WHERE username = '$user->username' AND password = '$user->password'";
-   $result = $conn->query($query);
+   
+   $stmt = $conn->prepare("SELECT * FROM Usuario WHERE username = ? AND password = ?");
+   $stmt->bind_param("ss", $user->username, $user->password);
+   $stmt->execute();
+   $stmt->bind_result($username, $password);
+   //$query = "SELECT username, password FROM Usuario WHERE username = '$user->username' AND password = '$user->password'";
+   //$result = $conn->query($query);
+   while ($stmt->fetch()) {
+        echo $username, $password;
+    }
+   $stmt->close();
 
-   $conn->close();
-
-   if ($result->num_rows == 0 || $result == FALSE) {
+   if ($username == FALSE) {
       return FALSE;
    }
    else {
@@ -27,7 +33,7 @@ function login(Usuario $user) {
 
 # just for tests
 function testLogin() {
-   $assoc = array('username' => 'fred@cleanic.com', 'password' => 'bla');
+   $assoc = array('username' => 'fred@cleanic.com', 'password' => 'fred');
 
    $user = new Usuario($assoc);
    print_r($user);
