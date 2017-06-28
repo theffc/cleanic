de1ce052599edc71dc808da59e6014ecde747924
Braz <pedroaugusto@alfaebeto.org.br>
2017-06-28 00:09:50 -0300

    API Agendamento Listagem

--- public/api/lista-horarios-disponiveis.php
+++ public/api/lista-horarios-disponiveis.php
@@ -3,9 +3,9 @@ require_once 'api-response.php';
 require_once '../../src/agendamento/agendamento-service.php';
 
 $json = file_get_contents('php://input');
-$assoc = json_decode($json, true)
+$assoc = json_decode($json, true);
 
-$result = ListHorariosDisponiveis($assoc['idMedico'], $assoc['dataAgendamento']);
+$result = ListaHorariosDisponiveis($assoc['idMedico'], $assoc['dataAgendamento']);
 
 if (!$result) {
    echo json_encode(new ApiResponse(false, "Could not retrieve schedule", NULL));
@@ -13,5 +13,4 @@ if (!$result) {
 }
 
 echo json_encode(new ApiResponse(true, "Schedule Found", $result));
-
 ?>
--- src/agendamento/agendamento-service.php
+++ src/agendamento/agendamento-service.php
@@ -1,25 +1,23 @@
 <?php
 require_once '../../config/defaults.php';
 require_once 'agendamento.php';
-require_once '../customer/customer-service.php';
 require_once 'AgendamentoModificado.php';
 require_once 'horarios-disponiveis.php';
 
 #return the appointments with Medics only, contains Medic name, speciality, pacient name and phone number
 function ListaAgendamento() {
-   $query = " SELECT	M.nomeFunc , M.especialidadeFunc , P.nomePac , P.telPac
+   $query = "SELECT	M.nomeFunc , M.especialidadeFunc , P.nomePac , P.telPac
             FROM Agenda AS A , Paciente AS P , Funcionario AS M
             WHERE A.codPaciente = P.codigoPac AND A.codFuncionario = M.idFunc AND M.cargoFunc = 'MEDICO'";
 
    $conn = connectToDatabase();
    $result = $conn->query($query);
-
    $agendamentosMod = array();
    while ($assoc = $result->fetch_assoc()) {
    	array_push($agendamentosMod, new AgendamentoModificado($assoc));
    }
 
-   return json_encode($agendamentosMod);
+   return $agendamentosMod;
 }
 
 function addAgendamento($requestAgendamento) {
@@ -61,7 +59,7 @@ class RequestNewAgendamento
    }
 }
 
-public function ListaHorariosDisponiveis($idMedico, $dataAgendamento){
+function ListaHorariosDisponiveis($idMedico, $dataAgendamento){
 
    $query = "SELECT horaAgendamento FROM Agenda AS A, Funcionario as M WHERE A.codFuncionario = '$idMedico' AND A.dataAgendamento = '$dataAgendamento' AND M.idFunc = '$idMedico' AND M.cargoFunc = 'MEDICO'";
    $db = connectToDatabase();
@@ -109,7 +107,7 @@ public function ListaHorariosDisponiveis($idMedico, $dataAgendamento){
          }
       }
       $horariosDisponiveis = new horariosDisponiveis($disponibilidades);
-      return json_encode($horariosDisponiveis);
+      return $horariosDisponiveis;
    }
 }
 
